3ad69ab9cc8bdffff31c80cd692b5cad3ccd38af
hshA12 <hsaidh123@gmail.com>
2026-02-14 01:26:47 +0200

    init

---
+++ Modules/UserIdentity/Domain/Entities/ValueOpjects/Email.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Domain.Entities.ValueOpjects
+{
+    public sealed class Email : IEquatable<Email>
+    {
+        private static readonly Regex EmailRegex = new(
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                     RegexOptions.Compiled | RegexOptions.IgnoreCase
+            );
+        private Email(string value)
+        {
+            Value = value;
+        }
+        public string Value { get; }
+
+        public bool Equals(Email? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
