11eda66fc74d157a2a8de1890aed9a0541fb866d
Yarakhatab <yarakhatab39@gmail.com>
2026-02-14 17:32:28 +0200

    Setup StorePlant module and migrations

---
+++ Infrastructure/Persistance/Data/StorePlant/StorePlantDbContext .cs
@@ -0,0 +1,22 @@
+﻿
+
+using Domain.Entities.StorePlant;
+
+namespace Persistance.Data.StorePlant
+{
+    public class StorePlantDbContext : DbContext
+    {
+        public StorePlantDbContext(DbContextOptions<StorePlantDbContext> options)
+       : base(options) { }
+
+        public DbSet<Plant> Plants => Set<Plant>();
+        public DbSet<UserPlant> UserPlants => Set<UserPlant>();
+        public DbSet<GrowthHistory> GrowthHistories => Set<GrowthHistory>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StorePlantDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
