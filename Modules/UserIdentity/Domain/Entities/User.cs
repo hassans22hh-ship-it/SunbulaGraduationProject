bc9f3467bd3b655190a99d3da3500e56aa4041b2
hshA12 <hsaidh123@gmail.com>
2026-02-15 17:54:35 +0200

    UserEntities

---
+++ Modules/UserIdentity/Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities.ValueOpjects;
+using SharedKernel;
+
+namespace Domain.Entities
+{
+    public class User:BaseEntity
+
+    {
+      private  User() { }
+        public User(Guid id,Email email,string firstName, string lastName, string ?phoneNumber )
+        {
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            IsActive = true;
+            IsEmailConfirmed = false;
+        }
+        public Email Email { get; private set; } = null!;
+        public string FirstName { get; private set; } = string.Empty;
+        public string LastName { get; private set; } = string.Empty;
+        public string? PhoneNumber { get; private set; }
+        public bool IsActive { get; private set; }
+        public bool IsEmailConfirmed { get; private set; }
+        public DateTime? LastLoginAt { get; private set; }
+
+    }
+}
--- Modules/UserIdentity/Domain/Entities/ValueOpjects/Email.cs
+++ Modules/UserIdentity/Domain/Entities/ValueOpjects/Email.cs
@@ -13,10 +13,36 @@ namespace Domain.Entities.ValueOpjects
             Value = value;
         }
         public string Value { get; }
-
+        public static Email Create(string email) 
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new ArgumentException("Email cannot be null or empty.", nameof(email));
+            }
+            var normalizedEmail = email.Trim().ToLowerInvariant();
+            if(!EmailRegex.IsMatch(normalizedEmail))
+            {
+                throw new ArgumentException("Invalid email format.", nameof(email));
+            }
+            if(normalizedEmail.Length > 254)
+            {
+                throw new ArgumentException("Email cannot be longer than 254 characters.", nameof(email));
+            }   
+            return new Email(normalizedEmail);
+        }
+        public static implicit operator string(Email email) => email.Value;
+     public   override  string ToString() => Value;
         public bool Equals(Email? other)
         {
-            throw new NotImplementedException();
+            if(other is null) return false;
+            if(ReferenceEquals(this, other)) return true;
+            return Value == other.Value;
         }
+        public override bool Equals(object? obj)
+     =>obj is Email email && Equals(email);
+        public static bool operator ==(Email? left, Email? right) =>
+     left?.Equals(right) ?? right is null;
+        public override int GetHashCode() => Value.GetHashCode();
+        public static bool operator !=(Email? left, Email? right) => !(left == right);
     }
 }
