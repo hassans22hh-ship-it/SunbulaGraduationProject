8f7186976872b4dfa438786df49418b4dd234339
HPNT11 <hassans22hh@gmail.com>
2026-03-18 07:03:01 +0200

    AddController

--- Modules/TaskManagement/Application/ServiceAbstraction/IFolderService.cs
+++ Modules/TaskManagement/Application/ServiceAbstraction/IFolderService.cs
@@ -4,11 +4,11 @@ namespace Application.ServiceAbstraction
 {
     public interface IFolderService
     {
-        Task<FolderDto> GetByIdAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
-        Task<FolderDto> GetByIdWithTasksAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
-        Task<IEnumerable<FolderDto>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
-        Task<FolderDto> CreateAsync(CreateFolderDto dto, Guid userId, CancellationToken cancellationToken = default);
-        Task<FolderDto> UpdateAsync(Guid id, UpdateFolderDto dto, Guid userId, CancellationToken cancellationToken = default);
-        Task DeleteAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
+     Task<FolderDto> GetByIdAsync(Guid id, Guid UserId, CancellationToken cancellationToken = default);
+    Task<FolderDto> GetByIdWithTasksAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<FolderDto>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<FolderDto> CreateAsync(CreateFolderDto dto, Guid userId, CancellationToken cancellationToken = default);
+    Task<FolderDto> UpdateAsync(Guid id, UpdateFolderDto dto, Guid userId, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
     }
 }
