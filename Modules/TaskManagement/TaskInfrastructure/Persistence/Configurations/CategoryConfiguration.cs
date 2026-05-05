5cd4ea0049b92425cee6a6aa3cf9963d0c61b8b4
Yarakhatab <yarakhatab39@gmail.com>
2026-05-05 20:41:18 +0300

    EditCategorty

--- Modules/TaskManagement/TaskInfrastructure/Persistence/Configurations/CategoryConfiguration.cs
+++ Modules/TaskManagement/TaskInfrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskDomain.Entities.TaskManagement;
 
 namespace TaskInfrastructure.Persistence.Configurations
 {
-    internal class CategoryConfiguration
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.OwnsOne(c => c.Color, color =>
+            {
+                color.Property(c => c.Value).HasColumnName("Color");
+            });
+        }
     }
 }
---
+++ Modules/TaskManagement/TaskInfrastructure/Persistence/Migrations/20260505173115_ConfigureCategoryColor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskInfrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureCategoryColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                schema: "task",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                schema: "task",
+                table: "Categories");
+        }
+    }
+}
